02a1569a986e0228a97787850657190d94f251c9
bertus-spec <bertus@student.umn.ac.id>
2025-12-04 22:19:17 +0700

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,5 +1,11 @@
 import joblib
+import streamlit as st 
+
+@st.cache_resource 
+def load_model():
+    model = joblib.load("knn_model.sav") 
+    return model
 
 def predict(data):
-      clf = joblib.load("knn_model_sav")
-  return clf.predict(data)
+    clf = load_model()
+    return clf.predict(data)
